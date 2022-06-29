9d9711c3446acae99bc6b74acfc8b4c35fb0ed86
jisu <jisuuoo96@gmail.com>
2022-06-30 00:42:12 +0900

    project init

---
+++ src/main/java/com/jisoolog/JisoologApplication.java
@@ -0,0 +1,13 @@
+package com.jisoolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JisoologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JisoologApplication.class, args);
+	}
+
+}
