8919d28ccd3156e35105d387f646d8aee679a8de
jisu <jisuuoo96@gmail.com>
2022-07-29 14:50:37 +0900

    test : post 테스트

--- src/test/java/com/jisoolog/api/controller/PostControllerTest.java
+++ src/test/java/com/jisoolog/api/controller/PostControllerTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,8 +21,13 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
     void test() throws Exception {
-        // expected
-        mockMvc.perform(get("/posts"))
+        // 보낼 데이터의 종류
+        // 글 제목
+        // 글 내용
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("title", "글 제목입니다.")
+                        .param("content", "글 내용입니다 하하")) // application/json
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
                 .andDo(print());
