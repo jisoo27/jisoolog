d7ea7b3fb7503a9fec8d67c31c1ec9f0bfd41f63
jisu <jisuuoo96@gmail.com>
2022-07-29 14:54:59 +0900

    feat : post Mapping - @RequestParam 이용하기   - Map으로 받기 - slf4j 이용하여 log.info로 log 찍어보기

--- src/main/java/com/jisoolog/api/controller/PostController.java
+++ src/main/java/com/jisoolog/api/controller/PostController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @Slf4j // 로그를 사용할 수 있음
 @RestController
 public class PostController {
@@ -15,9 +17,9 @@ public class PostController {
     // POST Method
 
     @PostMapping ("/posts")
-    public String post(@RequestParam String title, @RequestParam String content) {
+    public String post(@RequestParam Map<String, String> params) {
         // 실제 서버에서 받을 때에는 @RequestParam의 형태로 받을 수 있다.
-        log.info("title={}, content={}", title, content); // 로그 찍어보기
+        log.info("params={}", params); // 로그 찍어보기
         return "Hello World";
     }
 }
