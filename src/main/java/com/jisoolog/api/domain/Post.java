cc6c9edd1d0d76f01a1e931b3033c511cf934279
jisu <jisuuoo96@gmail.com>
2022-10-19 23:55:22 +0900

    feat: Post 엔티티 생성

---
+++ src/main/java/com/jisoolog/api/domain/Post.java
@@ -0,0 +1,25 @@
+package com.jisoolog.api.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity // JPA가 관리할 객체
+@NoArgsConstructor(access = AccessLevel.PUBLIC) // Protected 로 설정해 줄 경우 생성자가 생기지 않는다.
+public class Post {
+
+    @Id // 데이터베이스 primary key 와 매핑
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
