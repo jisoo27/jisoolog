764e8d7605c95dab66f227d9847c70f4cc2c03d3
jisu <jisuuoo96@gmail.com>
2022-10-20 00:00:56 +0900

    test: DB에 저장했을 경우 테스트 추가

--- src/test/java/com/jisoolog/api/controller/PostControllerTest.java
+++ src/test/java/com/jisoolog/api/controller/PostControllerTest.java
@@ -1,21 +1,28 @@
 package com.jisoolog.api.controller;
 
+import com.jisoolog.api.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
@@ -30,6 +37,7 @@ class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("{}"))
                 .andDo(print());
+        // db 에 post 2개 등록
     }
 
 
@@ -50,4 +58,22 @@ class PostControllerTest {
                 .andDo(print());
     }
 
+    @Test
+    @DisplayName("/posts 요청시 DB에 값이 저장된다.")
+    void test3() throws Exception {
+        // 보낼 데이터의 종류
+        // 글 제목
+        // 글 내용
+        // when
+        mockMvc.perform(post("/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\" : \"제목입니다.\", \"content\" : \"내용입니다.\"}")
+        )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // db -> post 1개 등록
+        // then
+        assertEquals(2L, postRepository.count()); // db -> post 2개 등록
+    }
 }
