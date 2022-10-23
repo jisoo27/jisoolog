fa6b9b9ff418d3f84e627c2050970371afece8bf
jisu <jisuuoo96@gmail.com>
2022-10-24 02:45:05 +0900

    feat: @builder 추가

--- src/main/java/com/jisoolog/api/request/PostCreate.java
+++ src/main/java/com/jisoolog/api/request/PostCreate.java
@@ -1,5 +1,6 @@
 package com.jisoolog.api.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,4 +18,9 @@ public class PostCreate { // DTO
     @NotBlank(message = "컨텐츠를 입력해주세요.")
     private String content;
 
+    @Builder
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
