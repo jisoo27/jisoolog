a2add7cc2a65d0481b4f86463aa6c9e4f15bf9a9
jisu <jisuuoo96@gmail.com>
2022-10-24 22:05:03 +0900

    feat: 글 단건 조회 API 구현

--- src/main/java/com/jisoolog/api/controller/PostController.java
+++ src/main/java/com/jisoolog/api/controller/PostController.java
@@ -1,12 +1,11 @@
 package com.jisoolog.api.controller;
 
+import com.jisoolog.api.domain.Post;
 import com.jisoolog.api.request.PostCreate;
 import com.jisoolog.api.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,4 +20,15 @@ public class PostController {
     public void post(@RequestBody @Valid PostCreate request) {
         postService.write(request);
     }
+
+    @GetMapping("/posts/{postId}")
+    public Post get(@PathVariable(name = "postId") Long id) {
+        Post post = postService.get(id);
+        return post;
+    }
+
 }
+
+/* /posts -> 글 전체 조회 (검색 + 페이징)
+ * /posts/{postId} -> 글 한개만 조회
+ */
